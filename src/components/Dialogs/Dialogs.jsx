6cca75eaab9233ca9c0358f47617924d7007fdd8
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-06-22 17:40:02 +0300

    new components Dialogs,ProfileInfo

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,35 @@
+import { NavLink } from "react-router-dom";
+import s from "./Dialogs.module.css";
+
+const DialogsItem = (props) => {
+    let path = "/dialogs/" + props.id;
+    return (
+        <NavLink to={path} className={navData => navData.isActive ? s.active : s.dialog}>{props.name}</NavLink>
+    )
+}
+
+const Message = (props) => {
+    return (
+        <div className={s.message}>
+            {props.message}
+        </div>
+    )
+}
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs_item}>
+                <DialogsItem  name="Dima" id="1"/>
+                <DialogsItem  name="Vova" id="2"/>
+                <DialogsItem  name="Ivan" id="3"/>
+                <DialogsItem  name="Roma" id="4"/>
+            </div>
+            <div className="messages">
+                <Message message="Hi"/>
+                <Message message="YEs"/>
+                <Message message="No"/>
+            </div>
         </div>
     )
 }
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,14 @@
+const ProfileInfo = () => {
+    return (
+        <div>
+            <div>
+                <img src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"></img>
+            </div>
+            <div>
+                ava
+            </div>
+        </div>
+    )
+}
+
+export default ProfileInfo;
