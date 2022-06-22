392c15732e3e603a9058fc15886d682b3364b678
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-06-22 17:40:45 +0300

    fix App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header.jsx';
+import Music from './components/Musiс/Music';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Profile from './components/Profile/Profile.jsx';
+import Settings from './components/Settings/Settings';
 
 const App = () => {
   return (
-    <div className="wrapper">
-      <Header />
-      <Navbar />
-      <div className='wrapper_content'>
-        <Profile />
+    <BrowserRouter>
+      <div className="wrapper">
+        <Header />
+        <div className='flex_content'>
+          <Navbar />
+          <div className='wrapper_content'>
+            <Routes>
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/dialogs/*" element={<Dialogs />} />
+              <Route path="/music" element={<Music />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </div>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
+
   );
 }
 
