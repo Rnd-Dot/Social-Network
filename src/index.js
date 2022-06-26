31439f5916a2323d7703be70f623b58bbc418373
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-06-26 16:46:11 +0300

    observer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import reportWebVitals from './reportWebVitals';
-import state from './state/state';
-import { rerenderEntireTree } from './render';
+import store from './state/state';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-rerenderEntireTree(state);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let rerenderEntireTree = (state) => {
+  root.render(
+    <React.StrictMode>
+      <App 
+      state={state} 
+      addPost={store.addPost.bind(store)} 
+      updateNewPostText={store.updateNewPostText.bind(store)}
+      updateNewMessageText={store.updateNewMessageText.bind(store)}
+      addMessage={store.addMessage.bind(store)}/>
+    </React.StrictMode>
+  );
+}
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
