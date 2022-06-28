30d0a4f1454b86b4a07cfee97e22dfde9dc5f975
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-06-28 14:13:32 +0300

    refactoring reducer

--- src/Redux/reducer-dialogs.js
+++ src/Redux/reducer-dialogs.js
@@ -1,17 +1,38 @@
-const dialogsReducer = (state, action) => {
-    if (action.type === "ADD-MESSAGE") {
-        let newMessageText = {
-            id: 4,
-            message: state.newMessages
-        };
-        state.messages.push(newMessageText);
-        state.newMessages = "";
-    }
-    else if(action.type === "UPDATE-NEW-MESSAGE-TEXT") {
-        state.newMessages = action.newText;
-    }
+let initialState = {
+    messages: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "New" },
+        { id: 3, message: "WOw" }
+    ],
 
-    return state;
+    
+
+    dialogs: [
+        { id: 1, name: "Dima" },
+        { id: 2, name: "Vova" },
+        { id: 3, name: "Ivan" },
+        { id: 4, name: "Roma" }
+    ],
+
+    newMessages: ""
+}
+
+const dialogsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD-MESSAGE":
+            let newMessageText = {
+                id: 4,
+                message: state.newMessages
+            };
+            state.messages.push(newMessageText);
+            state.newMessages = "";
+            return state;
+        case "UPDATE-NEW-MESSAGE-TEXT":
+            state.newMessages = action.newText;
+            return state;
+        default:
+            return state;
+    }
 }
 
 export default dialogsReducer;
--- src/Redux/reducer-profile.js
+++ src/Redux/reducer-profile.js
@@ -1,18 +1,31 @@
-const profileReducer = (state,action) => {
-    if(action.type === "ADD-POST") {
-        let newPost = {
-            id: 5,
-            message: state.newPostText,
-            like_count: 0
-        };
-        state.posts.push(newPost);
-        state.newPostText= "";
-    }
-    else if(action.type === "UPDATE-NEW-POST-TEXT") {
-        this._state.profilePage.newPostText = action.newText;
-    }
+let initialState = {
+    posts: [
+        { id: 1, message: "Здарова", like_count: "5" },
+        { id: 2, message: "Ты кто", like_count: "20" },
+        { id: 3, message: "Получилось", like_count: "100000" },
+        { id: 4, message: "так", like_count: "9" }
+    ],
 
-    return state;
+    newPostText: ""
+}
+
+const profileReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD-POST":
+            let newPost = {
+                id: 5,
+                message: state.newPostText,
+                like_count: 0
+            };
+            state.posts.push(newPost);
+            state.newPostText = "";
+            return state;
+        case "UPDATE-NEW-POST-TEXT":
+            state.newPostText = action.newText;
+            return state;
+        default:
+            return state;
+    }
 }
 
 export default profileReducer;
