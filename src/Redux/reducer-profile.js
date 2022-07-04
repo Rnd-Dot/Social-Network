6a5133a2931123ab63d4a01832048be5983fb433
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-07-04 12:18:22 +0300

    profile + new action creator setUserProfile

--- src/Redux/reducer-profile.js
+++ src/Redux/reducer-profile.js
@@ -1,5 +1,6 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+const SET_USER_PROFILE = "SET_USER_PROFILE";
 
 let initialState = {
     posts: [
@@ -9,30 +10,39 @@ let initialState = {
         { id: 4, message: "так", like_count: "9" }
     ],
 
-    newPostText: ""
+    newPostText: "",
+
+    profile: null
 }
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST: 
+        case ADD_POST:
             return {
                 ...state,
                 newPostText: "",
-                posts: [...state.posts, {id: 5, message:state.newPostText, like_count: 0}] 
+                posts: [...state.posts, { id: 5, message: state.newPostText, like_count: 0 }]
             }
         case UPDATE_NEW_POST_TEXT:
             return {
                 ...state,
                 newPostText: action.newText
             }
+        case SET_USER_PROFILE:
+            return {
+                ...state,
+                profile: action.profile
+            }
         default:
             return state;
-    
+
     }
 }
 
-export const addPostActionCreator = () => ({type: ADD_POST});
+export const addPostActionCreator = () => ({ type: ADD_POST });
 
-export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text});
+export const updateNewPostTextActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text });
+
+export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile });
 
 export default profileReducer;
