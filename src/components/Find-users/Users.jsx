6058d040dfd77b4a0247a58c52ee4e52ebda401e
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-07-04 12:17:43 +0300

    new NavLink Users

--- src/components/Find-users/Users.jsx
+++ src/components/Find-users/Users.jsx
@@ -1,14 +1,16 @@
 import userPhoto from "../../assets/images/user.png"
 import s from "./Users.module.css"
+import { NavLink } from "react-router-dom";
 
 const Users = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
-        
-        let pages = [];
 
-        for(let i = 1; i<= pagesCount ; i += 1) {
-            pages.push(i);
-        }
+    let pages = [];
+
+    for (let i = 1; i <= pagesCount; i += 1) {
+        pages.push(i);
+    }
+
 
     return <div>
         <div>
@@ -20,7 +22,9 @@ const Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={s.userPhoto} />
+                        <NavLink to={"/profile/" + u.id}>
+                            <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={s.userPhoto} />
+                        </NavLink>
                     </div>
                     <div>
                         {u.followed ?
