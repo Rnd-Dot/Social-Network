731318cba20853a321d4c75334a9064d973313b9
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-07-04 12:19:55 +0300

    new info users

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,11 +1,31 @@
-const ProfileInfo = () => {
+import Preloader from "../../common/Preloader";
+
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader />
+    }
     return (
         <div>
             <div>
                 <img src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"></img>
             </div>
             <div>
-                ava
+                <img src={props.profile.photos.large} />
+            </div>
+            <div>{props.profile.fullName}</div>
+            <span>{props.profile.aboutMe}</span>
+            <div>
+                <h2>Social networks:</h2>
+                <ul>
+                    <li>{props.profile.contacts.facebook}</li>
+                    <li>{props.profile.contacts.website}</li>
+                    <li>{props.profile.contacts.vk}</li>
+                    <li>{props.profile.contacts.twitter}</li>
+                    <li>{props.profile.contacts.instagram}</li>
+                    <li>{props.profile.contacts.youtube}</li>
+                    <li>{props.profile.contacts.github}</li>
+                    <li>{props.profile.contacts.mainLink}</li>
+                </ul>
             </div>
         </div>
     )
