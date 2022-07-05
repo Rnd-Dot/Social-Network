1e7a3f1ee7a227f06d6bca06cbbfc1bae840d9f9
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-07-05 08:28:06 +0300

    Header block

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
+import { NavLink } from "react-router-dom";
 import s from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
     <img src="https://1757140519.rsc.cdn77.org/static/v3/img/products/logo.png" alt=""></img>
+    <div className={s.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+    </div>
    </header>
-};
+}
+    
+
 
 export default Header;
