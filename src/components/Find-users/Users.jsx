f5582e4a392b506b2fdb13c41bb3603f67802a55
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-07-06 16:14:09 +0300

    follow/unfollow

--- src/components/Find-users/Users.jsx
+++ src/components/Find-users/Users.jsx
@@ -1,6 +1,7 @@
 import userPhoto from "../../assets/images/user.png"
 import s from "./Users.module.css"
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 const Users = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
@@ -28,8 +29,37 @@ const Users = (props) => {
                     </div>
                     <div>
                         {u.followed ?
-                            <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                            : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                            <button onClick={() => {
+                                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+                                    {
+                                        withCredentials: true,
+                                        headers: {
+                                            "API-KEY": "90d38e1f-b504-4d50-ba35-108bbc91a639"
+                                        }
+                                    }).then(response => {
+                                        if (response.data.resultCode === 0) {
+                                            props.unfollow(u.id)
+                                        }
+                                    });
+
+
+
+                            }}>
+                                Unfollow</button>
+                            : <button onClick={() => {
+                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {},
+                                    {
+                                        withCredentials: true,
+                                        headers: {
+                                            "API-KEY": "90d38e1f-b504-4d50-ba35-108bbc91a639"
+                                        }
+                                    }).then(response => {
+                                        if (response.data.resultCode === 0) {
+                                            props.follow(u.id)
+                                        }
+                                    });
+                            }}>
+                                Follow</button>}
                     </div>
                 </span>
                 <span>
