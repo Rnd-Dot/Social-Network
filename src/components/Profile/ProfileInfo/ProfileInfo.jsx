31af997834027ebb30568afe6aa314b8e354a255
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-07-10 18:08:21 +0300

    ref ProfileInfo

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,4 +1,6 @@
 import Preloader from "../../common/Preloader";
+import ProfileStatus from "./ProfileStatus/ProfileStatus"
+
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -9,24 +11,22 @@ const ProfileInfo = (props) => {
     return (
         <div>
             <div>
-                <img src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"></img>
-            </div>
-            <div>
                 <img src={props.profile.photos.large} />
             </div>
             <div>{props.profile.fullName}</div>
             <span>{props.profile.aboutMe}</span>
+            <ProfileStatus status={"aaaa"}/>
             <div>
                 <h2>Social networks:</h2>
                 <ul>
-                    <li>{props.profile.contacts.facebook}</li>
-                    <li>{props.profile.contacts.website}</li>
-                    <li>{props.profile.contacts.vk}</li>
-                    <li>{props.profile.contacts.twitter}</li>
-                    <li>{props.profile.contacts.instagram}</li>
-                    <li>{props.profile.contacts.youtube}</li>
-                    <li>{props.profile.contacts.github}</li>
-                    <li>{props.profile.contacts.mainLink}</li>
+                    <li>facebook:{props.profile.contacts.facebook}</li>
+                    <li>vk:{props.profile.contacts.vk}</li>
+                    <li>website:{props.profile.contacts.website}</li>
+                    <li>twitter:{props.profile.contacts.twitter}</li>
+                    <li>instagram:{props.profile.contacts.instagram}</li>
+                    <li>youtube:{props.profile.contacts.youtube}</li>
+                    <li>github:{props.profile.contacts.github}</li>
+                    <li>mainLink:{props.profile.contacts.mainLink}</li>
                 </ul>
             </div>
         </div>
