84cc09f5de0901a6b52b5c71e061e6564a60c4bb
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-07-13 18:52:14 +0300

    fix Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,16 @@ import { NavLink } from "react-router-dom";
 import s from "./Header.module.css";
 
 const Header = (props) => {
-    const exit = () => {
-        props.logout()
-    }
-
     return <header className={s.header}>
     <img src="https://1757140519.rsc.cdn77.org/static/v3/img/products/logo.png" alt=""></img>
     <div className={s.loginBlock}>
-        {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+        {props.isAuth ? 
+        <div>
+            {props.login}
+            <NavLink to={"/login"} onClick={props.logout}>logout</NavLink>
+        </div> 
+        : <NavLink to={"/login"}>Login</NavLink>}
     </div>
-    < NavLink onClick={exit} className={s.loginBlock} to={"/login"} >Exit</NavLink>
    </header>
 }
     
--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,13 +1,12 @@
 import Header from "./Header";
 import React from "react";
-import { getAuthUserData, logout } from "../../Redux/reducer-auth"
+import { logout } from "../../Redux/reducer-auth"
 import { connect } from "react-redux";
 
 
 class HeaderComponent extends React.Component{
-    componentDidMount() { 
-        this.props.getAuthUserData()
-    }
+    
+    
 
     render() {
         return <Header {...this.props}/>
@@ -19,4 +18,4 @@ let mapStateToProps = (state) => ({
     login: state.auth.login
 });
 
-export default connect(mapStateToProps,{getAuthUserData, logout}) (HeaderComponent);
+export default connect(mapStateToProps,{logout}) (HeaderComponent);
