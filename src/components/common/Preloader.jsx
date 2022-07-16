51f8829db4a177fec9244041843b6b243a8863fb
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-07-16 19:58:17 +0300

    preloader fix

--- src/components/common/Preloader.jsx
+++
@@ -1,9 +0,0 @@
-import loading from "../../assets/images/loading.gif"
-import s from "./Preloader.module.css"
-
-
-const Preloader = (props) => {
-    return <img src={loading} className={s.loading}/>
-}
-
-export default Preloader;
