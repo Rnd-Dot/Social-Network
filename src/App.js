f6b4c1c47ec9ab8408b296aa8f5ec774881ad276
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-07-17 12:32:19 +0300

    react.lazy + suspense

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import './App.css';
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Find-users/UsersContainer";
 import HeaderComponent from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import Music from './components/Musiс/Music';
 import Navbar from './components/Navbar/Navbar.jsx';
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import Settings from './components/Settings/Settings';
-import React from "react";
+import React, { Suspense } from "react";
 import { connect } from 'react-redux';
 import { initializeApp } from './Redux/reducer-app';
 import Preloader from './components/common/Preloader/Preloader';
 import { compose } from "redux";
 
-
+const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
+const UsersContainer = React.lazy(() => import("./components/Find-users/UsersContainer"));
 
 class App extends React.Component {
   componentDidMount() {
@@ -22,7 +21,7 @@ class App extends React.Component {
   }
 
   render() {
-    if(!this.props.initialized) {
+    if (!this.props.initialized) {
       return < Preloader />
     }
     return (
@@ -31,15 +30,17 @@ class App extends React.Component {
         <div className='flex_content'>
           <Navbar />
           <div className='wrapper_content'>
-            <Routes>
-              <Route path="/profile/:userId?" element={<ProfileContainer />} />
-              <Route path="/profile/*" element={<ProfileContainer />} />
-              <Route path="/dialogs/*" element={<DialogsContainer />} />
-              <Route path="/users/*" element={<UsersContainer />} />
-              <Route path="/music" element={<Music />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/settings" element={<Settings />} />
-            </Routes>
+            <Suspense fallback= {<Preloader />}>
+              <Routes>
+                <Route path="/profile/:userID" element={<ProfileContainer />} />
+                <Route path="/profile" element={<ProfileContainer />} />
+                <Route exact path="/dialogs" element={<DialogsContainer />} />
+                <Route path="/music" element={<Music />} />
+                <Route path="/users" element={<UsersContainer />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </Suspense>
           </div>
         </div>
       </div>
@@ -52,6 +53,6 @@ const mapStateToProps = (state) => ({
   initialized: state.app.initialized
 })
 
-export default compose (
-  connect(mapStateToProps, {initializeApp})) 
+export default compose(
+  connect(mapStateToProps, { initializeApp }))
   (App);
