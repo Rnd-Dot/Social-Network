c943f5b2f9eaf3d62c3b2a599f9d1d70e124e5cf
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-08-16 18:59:25 +0300

    common types

---
+++ src/types/types.ts
@@ -0,0 +1,37 @@
+export type postType = {
+    id: number
+    message: string
+    like_count: number
+}
+
+export type contactType = {
+    gitHub: string
+    vk: string
+    facebook: string
+    instagram: string
+    twitter: string
+    website: string
+    youtube: string
+    mainLink: string
+}
+
+export type photosType = {
+    small: string | null
+    large: string | null
+}
+
+export type profileType = {
+    userId: number
+    lookingForAJob: boolean
+    lookingForAJobDescription: string
+    fullName: string
+    contact: contactType
+    photos: photosType
+}
+
+export type UserType = {
+    id: number
+    name: string
+    status: string
+    photos: photosType
+}
