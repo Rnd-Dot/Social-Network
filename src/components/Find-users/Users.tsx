e45cc94d21f1474ef73f93162ef08d0a8b255eff
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-08-19 18:31:40 +0300

    ts users

---
+++ src/components/Find-users/Users.tsx
@@ -0,0 +1,33 @@
+import User from "./User";
+import React from "react";
+import { UserType } from "../../types/types";
+//@ts-ignore
+import Paginator from '../common/Paginator/Paginator.tsx';
+
+type Props = {
+    totalUsersCount: number
+    pageSize: number
+    onPageChanged: (pageNumber: number) => void
+    currentPage: number
+    users: Array<UserType>
+    followingProgress: Array<number>
+    followThunk: (id: number) => void
+    unfollowThunk: (id: number) => void
+}
+
+const Users: React.FC<Props> = ({ currentPage, onPageChanged, totalUsersCount, pageSize, ...props }) => {
+    return <div>
+        < Paginator totalItemCount={totalUsersCount} pageSize={pageSize}
+            currentPage={currentPage} onPageChanged={onPageChanged} />
+        {
+            props.users.map(u => <User key={u.id}
+                user={u}
+                followingProgress={props.followingProgress}
+                followThunk={props.followThunk}
+                unfollowThunk={props.unfollowThunk}
+                 />)
+        }
+    </div>
+}
+
+export default Users;
